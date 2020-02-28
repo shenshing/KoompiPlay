665ba67c31c69667b7e92413726e85dce873fa82
shenshing <lyshenshing20@gmail.com>
2020-02-28 00:20:20 -0500

    2nd commit : datetime with struct, use struct with route

--- userInfo/src/lib.rs
+++ userInfo/src/lib.rs
@@ -7,9 +7,15 @@
 // }
 
 
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+// #![feature(decl_macro)]
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
+// #[macro_use]
+// extern crate rocket;
 
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
@@ -26,29 +32,86 @@ pub fn establish_connection() -> PgConnection {
 }
 
 use chrono::NaiveDateTime;
-use self::models::{User, NewUser};
-pub fn insert_user(conn: &PgConnection,
-                       name: String,
-                       email: String,
-                       password: String,
-                       date: NaiveDateTime,
-                       profile:  Option<String>,
-                       role: Option<String>) {
+// use self::models::{User};
+// pub fn insert_user(conn: &PgConnection,
+//                        name: String,
+//                        email: String,
+//                        password: String,
+//                        date: String,
+//                        profile:  Option<String>,
+//                        role: Option<String>) {
+//     use schema::users;
+
+//     let new_user = User {
+//         user_name: name,
+//         user_email: email,
+//         user_password: password,
+//         create_date: date,
+//         user_profile: profile,
+//         user_role: role,
+//     };
+
+//     diesel::insert_into(users::table)
+//         .values(&new_user)
+//         .execute(conn)
+//         .expect("Error saving new user");
+// }
+
+
+use self::models::{User};
+pub fn insert_user(conn: &PgConnection, user: User) {
     use schema::users;
 
-    let new_user = NewUser {
-        user_name: name,
-        user_email: email,
-        user_password: password,
-        create_date: date,
-        user_profile: profile,
-        user_role: role,
+    let new_user = User {
+        user_name:      user.user_name,
+        user_email:     user.user_email,
+        user_password:  user.user_password,
+        create_date:    user.create_date,
+        user_profile:   user.user_profile,
+        user_role:      user.user_role
     };
 
     diesel::insert_into(users::table)
         .values(&new_user)
         .execute(conn)
         .expect("Error saving new user");
+}
+
+// extern crate userInfo;
+// use self::userInfo::*;
+// use std::env::args;
+
+// pub fn update_user(conn: &PgConnection,
+//                    table: self::schema::users::dsl::users,
+//                    ) {
+                  
+// }
+
+// use rocket::request::Form;
+extern crate rocket_contrib;
+use rocket_contrib::json::Json;
+
+#[post("/register", data = "<user>")]
+pub fn register(user: Json<User>) { 
+    // format!("{}\t{}\t{}\t{}\t{}\t{}", 
+    //     user.user_name,
+    //     user.user_email,
+    //     user.user_password,
+    //     user.create_date,
+    //     user.user_profile.as_ref().unwrap(),
+    //     user.user_role.as_ref().unwrap())
+
+    let connection = establish_connection();
+
+    let new_user = User {
+        user_name:      user.user_name.to_string(),
+        user_email:     user.user_email.to_string(),
+        user_password:  user.user_password.to_string(),
+        create_date:    user.create_date.to_string(),
+        user_profile:   user.user_profile.clone(),
+        user_role:      user.user_role.clone()
+    };
+    insert_user(&connection, new_user);
 }
 
 pub mod schema;
--- userInfo/src/models.rs
+++ userInfo/src/models.rs
@@ -1,7 +1,11 @@
+
+
 extern crate chrono;
 extern crate serde;
+
 use serde::{Serialize, Deserialize};
 use chrono::NaiveDateTime;
+
 
 // #[derive(Serialize, Deserialize, Debug)]
 // #[derive(Queryable, Debug, Serialize, Deserialize)]
@@ -15,28 +19,105 @@ use chrono::NaiveDateTime;
 //     pub user_profile: Option<&'a str>,
 //     pub user_role: Option<&'a str>,
 // }
-
-
+/*
+use diesel::pg::data_types::PgDate;
 #[derive(Queryable, Debug)]
 pub struct User {
     pub user_id: i32,
     pub user_name: String,
     pub user_email: String,
     pub user_password: String,
-    pub create_date: NaiveDateTime,
     pub user_profile: Option<String>,
     pub user_role: Option<String>,
 }
+*/
+// use chrono::naive::NaiveDateTime;
+use rocket::request::FromFormValue;
+use rocket::http::RawStr;
+use rocket::request::FromForm;
+// use chrono::Datelike;
+
+// impl FromForm for NaiveDateTime {
+
+// }
+// impl<'v, T: Datelike> FromFormValue<'v> for Option<T> {
+//     type Error = &'v RawStr;
+
+//     // fn from_form_value(form_value: &'v RawStr) -> Result<NaiveDateTime, &'v RawStr> {
+//     //     match form_value.parse::<NaiveDateTime>() {
+//     //         Ok(date) => Ok(NaiveDateTime{form_value}),
+//     //         _ => Err(form_value),
+//     //     }
+//     // }
+// }
+
+
+// struct AdultAge(usize);
+
+// impl<'v> FromFormValue<'v> for AdultAge {
+//     type Error = &'v RawStr;
+
+//     fn from_form_value(form_value: &'v RawStr) -> Result<AdultAge, &'v RawStr> {
+//         match form_value.parse::<usize>() {
+//             Ok(age) if age >= 21 => Ok(AdultAge(age)),
+//             _ => Err(form_value),
+//         }
+//     }
+// }
+
 
 use super::schema::users;
+use rocket::request::Form;
+// use serde::Deserialize;
 
-#[derive(Insertable)]
+
+#[derive(Queryable, Insertable, Deserialize)]
 #[table_name="users"]
-pub struct NewUser {
+pub struct User {
     pub user_name: String,
     pub user_email: String,
     pub user_password: String,
-    pub create_date: NaiveDateTime,
+    pub create_date: String,
     pub user_profile: Option<String>,
     pub user_role: Option<String>,
 }
+
+use std::io::Read;
+
+use rocket::{Request, Data, Outcome, Outcome::*};
+use rocket::data::{self, FromDataSimple};
+use rocket::http::{Status, ContentType};
+
+// Always use a limit to prevent DoS attacks.
+const LIMIT: u64 = 256;
+
+impl FromDataSimple for User {
+    type Error = String;
+
+    fn from_data(req: &Request, data: Data) -> data::Outcome<Self, String> {
+        
+        // let create_at = Utc::now();
+        // let create_at = Local::now().format("%A-%Y-%m-%d %H:%M:%S");
+        // println!("{}", date);
+        // Return successfully.
+        // Success(Person { name, age, create_at})
+
+        let profile = Some(String::from("user profile"));
+        let role = Some(String::from("user role"));
+            let new_user = User {
+                user_name: String::from("username"),
+                user_email: String::from("user email"),
+                user_password: String::from("user password"),
+                create_date: String::from("date time"),
+                user_profile: profile,
+                user_role: role,
+            };
+        Success(new_user)
+    }
+}
+
+// #[get("/item?<id>&<user..>")]
+// #[get("/login?<id>?<user..>")]
+// fn login(id: i32, user: Form<User>) {
+
+// }
