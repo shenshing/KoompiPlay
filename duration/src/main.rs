9001b5d5240dd7ec4f399ab81ff5e3e6d8c4aec9
shenshing <lyshenshing20@gmail.com>
2020-03-23 00:59:46 -0400

    duration for issue and expire date

--- duration/src/main.rs
+++ duration/src/main.rs
@@ -1,7 +1,11 @@
-use std::time::{Duration, SystemTime};
+use std::time::{SystemTime};
 use std::time::UNIX_EPOCH;
 
+extern crate chrono;
+use chrono::{DateTime, Utc};
+use chrono::Duration;
 fn main() {
+/*
     let time = std::time::SystemTime::now().duration_since(UNIX_EPOCH).expect("error");
     // let time1: i32 = time.parse().unwrap();
     // let time1: f64 = time.parse().unwrap();
@@ -13,4 +17,31 @@ fn main() {
     let f = Duration::new(5, 0);
     let f1 = f + Duration::new(0, 5);
     println!("{:?}", f1);
+
+
+    // let time = DateTime::now();
+
+    let now: DateTime<Utc> = Utc::now();
+
+    println!("{}", now);
+    let milli = now.timestamp_millis();
+    println!("{}", milli);
+*/
+    // let dmy = milli.from_timestamp();
+
+    // let time = Utc.timestamp_millis(1431648000).timestamp();
+    // println!("{:?}", time);
+
+
+//work withd DateTime<Utc>
+//oldDuration is the time that we issue and duration can be 1day, 1h, or whatever
+
+    let date:  DateTime<Utc> = Utc::now();
+    println!("{:?}", date);
+
+    let old_time = date;
+    let duration = Duration::days(1i64);
+    // let new_time = date.checked_add_signed(old_time);
+    let new_time = date.checked_add_signed(duration);
+    println!("{:?}", new_time.unwrap());
 }
