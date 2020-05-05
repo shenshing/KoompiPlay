032f4d97e91d6ce64d2bf62f7fda894346d58652
shenshing <lyshenshing20@gmail.com>
2020-05-05 05:06:23 -0400

    no blocking cors

--- main/src/main.rs
+++ main/src/main.rs
@@ -11,6 +11,10 @@ extern crate diesel;
 use self::userInfo::*;
 
 extern crate rocket_cors;
+
+// use rocket_contrib::templates::Template;
+use rocket_contrib::templates::Template;
+// use rocket_contrib::templates::Template;
 // use userInfo::{login, 
 //                admin_dashboard, 
 //                user_dashboard, 
@@ -19,9 +23,43 @@ extern crate rocket_cors;
 //                self_destroy,
 //                updateName};
 
+
+use rocket::http::Method;
+use rocket_cors::{
+    AllowedHeaders, AllowedOrigins, Error, 
+    Cors, CorsOptions 
+};
+
 fn main() {
 
-    let cors = rocket_cors::CorsOptions::default().to_cors().unwrap();;
+    // let cors = rocket_cors::CorsOptions::default()
+    //     // .allow_credentials(true)
+    //     .send_wildcard(true)
+    //     .to_cors().unwrap();
+
+
+    let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
+        // "http://localhost:8080",
+        // "http://127.0.0.1:8080",
+        // "http://localhost:8000",
+        // "http://0.0.0.0:8000",
+        "http://127.0.0.1:5500"
+    ]);
+
+    let cors = CorsOptions { 
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(), 
+        allowed_headers: AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Access-Control-Allow-Origin", 
+            "token",
+        ]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS");
 
     rocket::ignite()
         .mount("/", routes![register, 
@@ -39,12 +77,13 @@ fn main() {
                             displayUser,
                             userData,
                             test_token,
-                            upload_profile, //
+                            upload_profile, 
                             get_profile,
-			    test_login,
-			    userData1])
-                            // upload_to])
+			                test_login,
+			                userData1,
+                            userData2])
         .attach(cors)
+        .attach(Template::fairing())
         .launch();
 
 }
